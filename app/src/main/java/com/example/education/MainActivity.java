7fc21ecb188f5eb0c16994f12b52f09bf99d4b29
Alex <godnessdays365@gmail.com>
2020-03-28 22:17:53 +0200

    CrossActivity

--- app/src/main/java/com/example/education/MainActivity.java
+++ app/src/main/java/com/example/education/MainActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.util.PrintWriterPrinter;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = "MainActivity";
+    final static String EXTRA = "EXTRA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,59 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void move (View view ){
         Intent intent = new Intent(this, activity_1.class);
+        EditText editText = (EditText) findViewById(R.id.edittext);
+        String editText_buf = editText.getText().toString();
+        intent.putExtra(EXTRA, editText_buf);
         startActivity(intent);
     }
 
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
-        Toast.makeText(this, "onDestroy",Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.d(TAG, "onStop");
-        Toast.makeText(this, "onStop",Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Log.d(TAG, "onStart");
-        Toast.makeText(this, "onStart",Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.d(TAG, "onPause");
-        Toast.makeText(this, "onPause",Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Log.d(TAG, "onResume");
-        Toast.makeText(this, "onResume",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-        Log.d(TAG, "onRestart");
-        Toast.makeText(this, "onRestart",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected  void onSaveInstanceState(Bundle outState){
-        super.onSaveInstanceState(outState);
-        Log.d(TAG, "onSaveInstanceState");
-        Toast.makeText(this, "onSaveInstanceState",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected  void onRestoreInstanceState(Bundle savedInstanceState){
-        super.onRestoreInstanceState(savedInstanceState);
-        Log.d(TAG, "onRestoreInstanceState");
-        Toast.makeText(this, "onRestoreInstanceState",Toast.LENGTH_SHORT).show();
-    }
 
 }
--- app/src/main/java/com/example/education/activity_1.java
+++ app/src/main/java/com/example/education/activity_1.java
@@ -2,9 +2,11 @@ package com.example.education;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import com.example.education.MainActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class activity_1 extends AppCompatActivity {
 
@@ -12,7 +14,23 @@ public class activity_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
-        
+
+
+        Bundle argument = getIntent().getExtras();
+
+
+        //Intent intent = getIntent();
+        TextView textView = findViewById(R.id.textView);
+        try {
+            String message = argument.get(MainActivity.EXTRA).toString();
+
+            textView.setText(message);
+        }
+        catch (Exception ex)
+        {
+            textView.setText("do not work");
+        }
+
     }
 
 
